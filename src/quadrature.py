0dbda0be277dff74dc545d77d98caae238a27550
Greg Vernon <greg@coreform.com>
2022-09-08 20:54:52 -0600

    add: newton-cotes quadrature

--- src/quadrature.py
+++ src/quadrature.py
@@ -79,6 +79,77 @@ def riemannQuadrature( fun, num_points ):
         integral += fun( x_qp[i] ) * w_qp[i]
     return integral
 
+def getNewtonCotesQuadrature( num_points ):
+    if ( num_points < 1 ) or ( num_points > 6 ):
+        raise( Exception( "num_points_MUST_BE_INTEGER_IN_[1,6]" ) )
+    if num_points == 1:
+        x_qp = numpy.array( [0.0] )
+        w_qp = numpy.array( [2.0] )
+    elif num_points == 2:
+        x_qp = numpy.array( [-1.0, +1.0] )
+        w_qp = numpy.array( [1.0, 1.0] )
+    elif num_points == 3:
+        x_qp = numpy.array( [-1.0, 0.0, +1.0] )
+        w_qp = numpy.array( [1.0, 4.0, 1.0] ) / 3.0
+    elif num_points == 4:
+        x_qp = numpy.array( [-1.0, -1.0/3.0, +1.0/3.0, +1.0] )
+        w_qp = numpy.array( [1.0, 3.0, 3.0, 1.0] ) / 4.0
+    elif num_points == 5:
+        x_qp = numpy.array( [-1.0, -0.5, 0.0, +0.5, +1.0] )
+        w_qp = numpy.array( [7.0, 32.0, 12.0, 32.0, 7.0] ) / 45.0
+    elif num_points == 6:
+        x_qp = numpy.array( [-1.0, -0.6, -0.2, +0.2, +0.6, +1.0] )
+        w_qp = numpy.array( [19.0, 75.0, 50.0, 50.0, 75.0, 19.0] ) / 144.0
+    return x_qp, w_qp
+
+def computeNewtonCotesQuadrature( fun, num_points ):
+    x_qp, w_qp = getNewtonCotesQuadrature( num_points = num_points )
+    integral = 0.0
+    for i in range( 0, len( x_qp ) ):
+        integral += fun( x_qp[i] ) * w_qp[i]
+    return integral
+
+class Test_computeNewtonCotesQuadrature( unittest.TestCase ):
+    def test_integrate_constant_one( self ):
+        constant_one = lambda x : 1 * x**0
+        for degree in range( 1, 6 ):
+            num_points = degree + 1
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = constant_one, num_points = num_points ), second = 2.0, delta = 1e-12 )
+            
+    def test_exact_poly_int( self ):
+        for degree in range( 1, 6 ):
+            num_points = degree + 1
+            poly_fun = lambda x : ( x + 1.0 ) ** degree
+            indef_int = lambda x : ( ( x + 1 ) ** ( degree + 1) ) / ( degree + 1 )
+            def_int = indef_int(1.0) - indef_int(-1.0)
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = poly_fun, num_points = num_points ), second = def_int, delta = 1e-12 )
+
+    def test_integrate_sin( self ):
+        sin = lambda x : math.sin(x)
+        for num_points in range( 1, 7 ):
+            self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = sin, num_points = num_points ), second = 0.0, delta = 1e-12 )
+    
+    def test_integrate_cos( self ):
+        cos = lambda x : math.cos(x)
+        self.assertAlmostEqual( first = computeNewtonCotesQuadrature( fun = cos, num_points = 6 ), second = 2*math.sin(1), delta = 1e-4 )
+
+class Test_getNewtonCotesQuadrature( unittest.TestCase ):
+    def test_incorrect_num_points( self ):
+        with self.assertRaises( Exception ) as context:
+            getNewtonCotesQuadrature( num_points = 0 )
+        self.assertEqual( "num_points_MUST_BE_INTEGER_IN_[1,6]", str( context.exception ) )
+        with self.assertRaises( Exception ) as context:
+            getNewtonCotesQuadrature( num_points = 7 )
+        self.assertEqual( "num_points_MUST_BE_INTEGER_IN_[1,6]", str( context.exception ) )
+                
+    def test_return_types( self ):
+        for num_points in range( 1, 7 ):
+            x, w = getNewtonCotesQuadrature( num_points = num_points )
+            self.assertIsInstance( obj = x, cls = numpy.ndarray )
+            self.assertIsInstance( obj = w, cls = numpy.ndarray )
+            self.assertTrue( len( x ) == num_points )
+            self.assertTrue( len( w ) == num_points )
+
 class Test_computeRiemannQuadrature( unittest.TestCase ):
     def test_integrate_constant_one( self ):
         constant_one = lambda x : 1
