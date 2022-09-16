8b64b372167f6f13ff65bd6d06aa250896a877ce
Greg Vernon <greg.j.vernon@gmail.com>
2022-09-15 19:56:47 -0600

    chore: rename tests

--- src/approx.py
+++ src/approx.py
@@ -36,22 +36,22 @@ def computeFitError( target_fun, coeff, node_coords, ien_array, eval_basis ):
     return fit_error, residual
 
 class Test_computeSolution( unittest.TestCase ):
-    def test_single_element_linear_poly( self ):
+    def test_single_linear_element_poly( self ):
         test_solution, _, _ = computeSolution( target_fun = lambda x : x, domain = [-1.0, 1.0 ], num_elems = 1, degree = 1 )
         gold_solution = numpy.array( [ -1.0, 1.0 ] )
         self.assertTrue( numpy.allclose( test_solution, gold_solution ) )
     
-    def test_single_element_quad_poly( self ):
+    def test_single_quad_element_poly( self ):
         test_solution, _, _ = computeSolution( target_fun = lambda x : x**2, domain = [-1.0, 1.0 ], num_elems = 1, degree = 2 )
         gold_solution = numpy.array( [ 1.0, 0.0, 1.0 ] )
         self.assertTrue( numpy.allclose( test_solution, gold_solution ) )
     
-    def test_two_element_linear_poly( self ):
+    def test_two_linear_element_poly( self ):
         test_solution, _, _ = computeSolution( target_fun = lambda x : x**2, domain = [-1.0, 1.0 ], num_elems = 2, degree = 1 )
         gold_solution = numpy.array( [ 1.0, 0.0, 1.0 ] )
         self.assertTrue( numpy.allclose( test_solution, gold_solution ) )
     
-    def test_four_element_linear_poly( self ):
+    def test_four_quad_element_poly( self ):
         test_solution, _, _ = computeSolution( target_fun = lambda x : x**2, domain = [-1.0, 1.0 ], num_elems = 4, degree = 1 )
         gold_solution = numpy.array( [ 1.0, 0.25, 0.0, 0.25, 1.0 ] )
         self.assertTrue( numpy.allclose( test_solution, gold_solution ) )
