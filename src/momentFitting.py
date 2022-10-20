17fce925902535b6951923386a57e52d2be20116
Greg Vernon <greg.j.vernon@gmail.com>
2022-10-20 10:55:19 -0600

    refactor: moment fitting generalization

--- src/momentFitting.py
+++ src/momentFitting.py
@@ -3,55 +3,59 @@ from scipy import optimize
 import numpy
 import unittest
 
-if __name__ == "problems.moment_fitting":
-    from problems import basis
-elif __name__ == "moment_fitting":
+if __name__ == "src.momentFitting":
+    from src import basis
+elif __name__ == "momentFitting":
     import basis
 
-def computeGaussLegendreQuadrature( n ):
-    M = numpy.zeros( 2*n, dtype = "double" )
-    M[0] = 2.0
-    x0 = numpy.linspace( -1, 1, n )
-    sol = scipy.optimize.least_squares( lambda x : objFun( M, x ), x0, bounds = (-1, 1), ftol = 1e-14, xtol = 1e-14, gtol = 1e-14 )
+def computeQuadrature( n, domain, generating_basis ):
+    M = computeMomentVector( n, domain, generating_basis )
+    x0 = numpy.linspace( domain[0], domain[1], n )
+    sol = scipy.optimize.least_squares( lambda x : objFun( M, generating_basis, x ), x0, bounds = (-1, 1), ftol = 1e-14, xtol = 1e-14, gtol = 1e-14 )
     qp = sol.x
-    w = solveLinearMomentFit( M, qp )
+    w = solveLinearMomentFit( M, generating_basis, qp )
     return qp, w 
 
-def assembleLinearMomentFitSystem( degree, pts ):
+def computeMomentVector( n, domain, generating_basis ):
+    moment_vector = numpy.zeros( 2*n )
+    for i in range( 0, 2*n ):
+        moment_vector[i], _ = scipy.integrate.quad( lambda x: generating_basis( i, x ), domain[0], domain[1], epsrel = 1e-12, limit = 1000 )
+    return moment_vector
+
+def assembleLinearMomentFitSystem( degree, generating_basis, pts ):
     A = numpy.zeros( shape = ( degree + 1, len( pts ) ), dtype = "double" )
     for p in range( 0, degree + 1 ):
         for i in range( 0, len( pts ) ):
-            A[p,i] = basis.evalLegendreBasis1D( p,  pts[i] )
+            A[p,i] = generating_basis( p,  pts[i] )
     return A
 
-def solveLinearMomentFit( M, pts ):
+def solveLinearMomentFit( M, generating_basis, pts ):
     degree = len( M ) - 1
-    A = assembleLinearMomentFitSystem( degree, pts )
+    A = assembleLinearMomentFitSystem( degree, generating_basis, pts )
     sol = scipy.optimize.lsq_linear( A, M )
     w = sol.x
     return w
 
-def objFun( M, pts ):
+def objFun( M, generating_basis, pts ):
     degree = len( M ) - 1
-    A = assembleLinearMomentFitSystem( degree, pts )
-    w = solveLinearMomentFit( M, pts )
+    A = assembleLinearMomentFitSystem( degree, generating_basis, pts )
+    w = solveLinearMomentFit( M, generating_basis, pts )
     obj = ( M - A @ w )
     obj = obj.squeeze()
-    print(A.shape, w.shape, obj.shape)
     return obj
 
 class Test_computeGaussLegendreQuadrature( unittest.TestCase ):
     def test_1_pt( self ):
         qp_gold = numpy.array( [ 0.0 ] )
         w_gold = numpy.array( [ 2.0 ] )
-        [ qp, w ] = computeGaussLegendreQuadrature( 1 )
+        [ qp, w ] = computeQuadrature( 1, [-1, 1], basis.evalLegendreBasis1D )
         self.assertAlmostEqual( first = qp, second = qp_gold, delta = 1e-12 )
         self.assertAlmostEqual( first = w, second = w_gold, delta = 1e-12 )
         
     def test_2_pt( self ):
         qp_gold = numpy.array( [ -1.0/numpy.sqrt(3), 1.0/numpy.sqrt(3) ] )
         w_gold = numpy.array( [ 1.0, 1.0 ] )
-        [ qp, w ] = computeGaussLegendreQuadrature( 2 )
+        [ qp, w ] = computeQuadrature( 2, [-1, 1], basis.evalLegendreBasis1D )
         self.assertTrue( numpy.allclose( qp, qp_gold ) )
         self.assertTrue( numpy.allclose( w, w_gold ) )
     
@@ -62,7 +66,7 @@ class Test_computeGaussLegendreQuadrature( unittest.TestCase ):
         w_gold = numpy.array( [ 5.0 / 9.0, 
                                 8.0 / 9.0, 
                                 5.0 / 9.0 ] )
-        [ qp, w ] = computeGaussLegendreQuadrature( 3 )
+        [ qp, w ] = computeQuadrature( 3, [-1, 1], basis.evalLegendreBasis1D )
         self.assertTrue( numpy.allclose( qp, qp_gold ) )
         self.assertTrue( numpy.allclose( w, w_gold ) )
     
@@ -75,7 +79,7 @@ class Test_computeGaussLegendreQuadrature( unittest.TestCase ):
                                 ( 18.0 + numpy.sqrt( 30.0 ) ) / 36.0,
                                 ( 18.0 + numpy.sqrt( 30.0 ) ) / 36.0,
                                 ( 18.0 - numpy.sqrt( 30.0 ) ) / 36.0 ] )
-        [ qp, w ] = computeGaussLegendreQuadrature( 4 )
+        [ qp, w ] = computeQuadrature( 4, [-1, 1], basis.evalLegendreBasis1D )
         self.assertTrue( numpy.allclose( qp, qp_gold ) )
         self.assertTrue( numpy.allclose( w, w_gold ) )
     
@@ -90,6 +94,6 @@ class Test_computeGaussLegendreQuadrature( unittest.TestCase ):
                                   128.0 / 225.0,
                                 ( 322.0 + 13.0 * numpy.sqrt( 70.0 ) ) / 900.0,
                                 ( 322.0 - 13.0 * numpy.sqrt( 70.0 ) ) / 900.0, ] )
-        [ qp, w ] = computeGaussLegendreQuadrature( 5 )
+        [ qp, w ] = computeQuadrature( 5, [-1, 1], basis.evalLegendreBasis1D )
         self.assertTrue( numpy.allclose( qp, qp_gold ) )
         self.assertTrue( numpy.allclose( w, w_gold ) )
