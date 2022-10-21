3e7bbb2e39df973b7d0b8984a73ed4a73ae35597
Greg Vernon <greg.j.vernon@gmail.com>
2022-10-21 10:48:20 -0600

    refactor: update tests for galerkin approximation

--- src/approxGalerkin.py
+++ src/approxGalerkin.py
@@ -70,21 +70,41 @@ def plotCompareGoldTestSolution( gold_coeff, test_coeff, domain, solution_basis 
     plt.show()
 
 class Test_computeSolution( unittest.TestCase ):
-    def test_polynomial_target( self ):
+    def test_cubic_polynomial_target( self ):
+        # print( "POLY TEST" )
         target_fun = lambda x: x**3 - (8/5)*x**2 + (3/5)*x
-        test_sol_coeff = computeSolution( target_fun = target_fun, domain = [0, 1], degree = 2, solution_basis = basis.evalBernsteinBasis1D )
+        domain = [ 0, 1 ]
+        degree = 2
+        solution_basis = basis.evalBernsteinBasis1D
+        test_sol_coeff = computeSolution( target_fun = target_fun, domain = domain, degree = degree, solution_basis =solution_basis )
         gold_sol_coeff = numpy.array( [ 1.0 / 20.0, 1.0 / 20.0, -1.0 / 20.0 ] )
+        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
         self.assertTrue( numpy.allclose( gold_sol_coeff, test_sol_coeff ) )
-    
+        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-12 )
+
     def test_sin_target( self ):
+        # print( "SIN TEST" )
         target_fun = lambda x: numpy.sin( numpy.pi * x )
-        test_sol_coeff = computeSolution( target_fun = target_fun, domain = [0, 1], degree = 2, solution_basis = basis.evalBernsteinBasis1D )
+        domain = [ 0, 1 ]
+        degree = 2
+        solution_basis = basis.evalBernsteinBasis1D
+        test_sol_coeff = computeSolution( target_fun = target_fun, domain = domain, degree = degree, solution_basis = solution_basis )
         gold_sol_coeff = numpy.array( [ (12*(numpy.pi**2 - 10))/(numpy.pi**3), -(6*(3*numpy.pi**2 - 40))/(numpy.pi**3), (12*(numpy.pi**2 - 10))/(numpy.pi**3)] )
-        self.assertTrue( numpy.allclose( gold_sol_coeff, test_sol_coeff ) )
+        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [0, 1], solution_basis )
+        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-5 )
         
     def test_erfc_target( self ):
-        target_fun = lambda x: scipy.special.erfc( x )
-        test_sol_coeff = computeSolution( target_fun = target_fun, domain = [-2, 2], degree = 3, solution_basis = basis.evalBernsteinBasis1D )
+        # print( "ERFC TEST" )
+        target_fun = lambda x: numpy.real( scipy.special.erfc( x ) )
+        domain = [ -2, 2 ]
+        degree = 3
+        solution_basis = basis.evalBernsteinBasis1D
+        test_sol_coeff = computeSolution( target_fun = target_fun, domain = domain, degree = degree, solution_basis = solution_basis )
         gold_sol_coeff = numpy.array( [ 1.8962208131568558391841630949727, 2.6917062016799657617278998883219, -0.69170620167996576172789988832194, 0.10377918684314416081583690502732] )
-        self.assertTrue( numpy.allclose( gold_sol_coeff, test_sol_coeff ) )
+        fit_err = computeFitError( gold_sol_coeff, test_sol_coeff, domain, solution_basis )
+        # plotCompareGoldTestSolution( gold_sol_coeff, test_sol_coeff, [-2, 2], solution_basis )
+        self.assertAlmostEqual( first = fit_err, second = 0, delta = 1e-4 )
+    
     
