876047fbc4adad9d92b2f5d0fdad05ecc933992a
gvernon2 <greg.j.vernon@gmail.com>
2022-11-16 13:05:46 -0700

    test: gram matrix tests

--- src/splineApproxGalerkin.py
+++ src/splineApproxGalerkin.py
@@ -251,28 +251,41 @@ if __name__ == "__main__":
 #         self.assertAlmostEqual( evaluateSolutionAt( x =  1.0, domain = [0.0, 1.0], coeff = coeff, solution_basis = basis.evalLagrangeBasis1D ), 2.0 )
 
 class Test_assembleGramMatrix( unittest.TestCase ):
-    def test_linear_two_element_quadratic_bspline( self ):
+    def test_two_element_linear_bspline( self ):
         target_fun = lambda x: x**0
-        spline_space = { "domain": [0, 1], "degree": [ 2, 2 ], "continuity": [ -1, 1, -1 ] }
+        spline_space = { "domain": [0, 2], "degree": [ 1, 1 ], "continuity": [ -1, 0, -1 ] }
         uspline.make_uspline_mesh( spline_space, "temp_uspline" )
         uspline_bext = bext.readBEXT( "temp_uspline.json" )
         test_gram_matrix = assembleGramMatrix( uspline_bext = uspline_bext )
-        gold_gram_matrix = numpy.array( [ [ 1.0/10.0, 7.0/120.0, 1.0/120.0, 0.0 ], 
-                                          [ 7.0/120.0, 1.0/6.0, 1.0/10.0, 1.0/120.0],
-                                          [ 1.0/120.0, 1.0/10.0, 1.0/6.0, 7.0/120.0 ],
-                                          [ 0.0, 1.0/120.0, 7.0/120.0, 1.0/10.0] ] )
-        print( test_gram_matrix )
+        gold_gram_matrix = numpy.array( [ [ 1.0/3.0, 1.0/6.0, 0.0 ], 
+                                          [ 1.0/6.0, 2.0/3.0, 1.0/6.0 ],
+                                          [ 0.0, 1.0/6.0, 1.0/3.0 ] ] )
         self.assertTrue( numpy.allclose( test_gram_matrix, gold_gram_matrix ) )
-
-#     def test_quadratic_two_element_quadratic_bspline( self ):
-#         test_gram_matrix = assembleGramMatrix( domain = [0, 1], degree = 2, solution_basis = basis.evalBernsteinBasis1D )
-#         gold_gram_matrix = numpy.array( [ [0.2, 0.1, 1.0/30.0], [0.1, 2.0/15.0, 0.1], [1.0/30.0, 0.1, 0.2] ] )
-#         self.assertTrue( numpy.allclose( test_gram_matrix, gold_gram_matrix ) )
     
-#     def test_cubic_two_element_quadratic_bspline( self ):
-#         test_gram_matrix = assembleGramMatrix( domain = [0, 1], degree = 3, solution_basis = basis.evalBernsteinBasis1D )
-#         gold_gram_matrix = numpy.array( [ [1.0/7.0, 1.0/14.0, 1.0/35.0, 1.0/140.0], [1.0/14.0, 3.0/35.0, 9.0/140.0, 1.0/35.0], [1.0/35.0, 9.0/140.0, 3.0/35.0, 1.0/14.0], [ 1.0/140.0, 1.0/35.0, 1.0/14.0, 1.0/7.0] ] )
-#         self.assertTrue( numpy.allclose( test_gram_matrix, gold_gram_matrix ) )
+    def test_two_element_quadratic_bspline( self ):
+        target_fun = lambda x: x**0
+        spline_space = { "domain": [0, 2], "degree": [ 2, 2 ], "continuity": [ -1, 1, -1 ] }
+        uspline.make_uspline_mesh( spline_space, "temp_uspline" )
+        uspline_bext = bext.readBEXT( "temp_uspline.json" )
+        test_gram_matrix = assembleGramMatrix( uspline_bext = uspline_bext )
+        gold_gram_matrix = numpy.array( [ [ 1.0/5.0, 7.0/60.0, 1.0/60.0, 0.0 ], 
+                                          [ 7.0/60.0, 1.0/3.0, 1.0/5.0, 1.0/60.0],
+                                          [ 1.0/60.0, 1.0/5.0, 1.0/3.0, 7.0/60.0 ],
+                                          [ 0.0, 1.0/60.0, 7.0/60.0, 1.0/5.0] ] )
+        self.assertTrue( numpy.allclose( test_gram_matrix, gold_gram_matrix ) )
+    
+    def test_two_element_cubic_bspline( self ):
+        target_fun = lambda x: x**0
+        spline_space = { "domain": [0, 2], "degree": [ 3, 3 ], "continuity": [ -1, 2, -1 ] }
+        uspline.make_uspline_mesh( spline_space, "temp_uspline" )
+        uspline_bext = bext.readBEXT( "temp_uspline.json" )
+        test_gram_matrix = assembleGramMatrix( uspline_bext = uspline_bext )
+        gold_gram_matrix = numpy.array( [ [ 1.0/7.0, 7.0/80.0, 1.0/56.0, 1.0/560.0, 0.0 ], 
+                                          [ 7.0/80.0, 31.0/140.0, 39.0/280.0, 1.0/20.0, 1.0/560.0 ],
+                                          [ 1.0/56.0, 39.0/280.0, 13.0/70.0, 39.0/280.0, 1.0/56.0 ],
+                                          [ 1.0/560.0, 1.0/20.0, 39.0/280.0, 31.0/140.0, 7.0/80.0 ],
+                                          [ 0.0, 1.0/560.0, 1.0/56.0, 7.0/80.0, 1.0/7.0 ] ] )
+        self.assertTrue( numpy.allclose( test_gram_matrix, gold_gram_matrix ) )
 
 # class Test_assembleForceVector( unittest.TestCase ):
 #     def test_const_force_fun_two_element_quadratic_bspline( self ):
